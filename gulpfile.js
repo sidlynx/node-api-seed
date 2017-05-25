ceec05b6067d431a20d8d1ef952c2434b804e989
Mohammed Hamdoune <sidlynx@gmail.cm>
2017-05-25 23:36:03 +0000

    gulp first task(tsc -w)

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require("gulp");
+
+
+gulp.task("0-ts", function (callback) {
+    "use strict";
+    var spawn = require('child_process').spawn;
+    var ls;
+    if (/^win/.test(process.platform)) {
+        ls = spawn('cmd.exe', ['/c', 'tsc -w']);
+    }
+    else {
+        ls = spawn('sh', ['-c', 'tsc']);
+    }
+
+
+    ls.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+    });
+
+    ls.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+
+    ls.on('exit', function (code) {
+        console.log('child process exited with code ' + code);
+    });
+});
