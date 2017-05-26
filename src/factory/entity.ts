d31dc1456ec5a4cb93e37f32d27743c9d562c782
Mohammed Hamdoune <sidlynx@gmail.com>
2017-05-26 11:47:38 +0000

    add entity factory

---
+++ src/factory/entity.ts
@@ -0,0 +1,22 @@
+import { Entity } from "../entity/entity";
+import { Factory } from "./factory";
+
+export class EntityFactory<T extends Entity> extends Factory{
+
+    save(t:T){
+
+    }
+    findOneByGuid(guid:string){
+
+    }
+    update(t:T){
+
+    }
+    deleteUnsafe(t:T){
+        
+    }
+
+    isSaved(t:T){
+
+    }
+}
